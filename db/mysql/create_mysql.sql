ca8b9a435f51747e2ebb5fb7026779dfe4f50f4d
gonzalovazquez <gonzalovazquez010@gmail.com>
2017-12-19 10:00:31 -0500

    Added new field to database

--- db/mysql/create_mysql.sql
+++ db/mysql/create_mysql.sql
@@ -6,14 +6,16 @@ DROP TABLE IF EXISTS chore;
 
 CREATE TABLE chore (
   id VARCHAR(32) NOT NULL,
+  customer_id VARCHAR(32) NOT NULL,
   name VARCHAR(128) NOT NULL,
   recurrent VARCHAR(128),
   completed BIT DEFAULT 0 NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO chore (id, name, recurrent, completed) VALUES ('1', 'Wash car', 'Weekly', 1);
-INSERT INTO chore (id, name, recurrent, completed) VALUES ('2', 'Do the dishes', 'Daily', 0);
-INSERT INTO chore (id, name, recurrent, completed) VALUES ('3', 'Do Laundary', 'Monthly', 0);
-INSERT INTO chore (id, name, recurrent, completed) VALUES ('4', 'Walk the dog', 'Daily', 1);
-INSERT INTO chore (id, name, recurrent, completed) VALUES ('5', 'Visit grandma', 'Weekly', 0);
+INSERT INTO chore (id, customer_id, name, recurrent, completed) VALUES ('1', '1', 'Wash car', 'Weekly', 1);
+INSERT INTO chore (id, customer_id, name, recurrent, completed) VALUES ('2', '1', 'Do the dishes', 'Daily', 0);
+INSERT INTO chore (id, customer_id, name, recurrent, completed) VALUES ('3', '2', 'Do Laundary', 'Monthly', 0);
+INSERT INTO chore (id, customer_id, name, recurrent, completed) VALUES ('4', '2', 'Walk the dog', 'Daily', 1);
+INSERT INTO chore (id, customer_id, name, recurrent, completed) VALUES ('5', '3','Visit grandma', 'Weekly', 0);
+INSERT INTO chore (id, customer_id, name, recurrent, completed) VALUES ('6', '3','Brush my teeth', 'Daily', 0);
